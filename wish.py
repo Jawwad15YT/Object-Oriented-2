11ef01300f8fdeb2b2ab76cab151efff144c9536
Jawwad15YT <53828972+Jawwad15YT@users.noreply.github.com>
2024-10-28 22:53:24 +0600

    Add files via upload

---
+++ wish.py
@@ -0,0 +1,11 @@
+class wish:
+    def gm(self):
+        print("Good Morning")
+    def gn(self):
+        print("Good Night")
+    def ny(self):
+        print("Happy New Years!")
+
+Obj = wish()
+Obj.ny()
+Obj.gm()
